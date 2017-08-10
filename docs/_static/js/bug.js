f04114ccb4056ba1fff48ae2214c6b4ee6949ccd
Umesh Singla <umeshksingla@gmail.com>
2017-08-11 05:21:20 +0530

    Add check login prototype function

--- docs/_static/js/bug.js
+++ docs/_static/js/bug.js
@@ -1,7 +1,27 @@
 console.log('umesh');
 console.log(window.location.href);
 
-function toggleFeedbackForm(){
+function toggleFeedbackForm() {
     console.log("i'm playing");
     document.getElementById("myForm").classList.toggle("show");
+    check_login();
+}
+
+function check_login() {
+
+    var logged_in = false;
+    console.log('checking login');
+
+    var submit_btn= document.getElementById("submit-btn");
+    var login_btn= document.getElementById("login-btn");
+
+    // TODO: call JIRA API
+
+    if (logged_in) {
+        submit_btn.style.display = 'block';
+        login_btn.style.display = 'none';
+    } else {
+        submit_btn.style.display = 'none';
+        login_btn.style.display = 'block';
+    }
 }
