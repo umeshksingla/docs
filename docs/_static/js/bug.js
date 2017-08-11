7396ece6807cd879830cf03e8731731eca86d3f1
Umesh Singla <umeshksingla@gmail.com>
2017-08-11 08:09:07 +0530

    Add login and check session APIs to bug-form

--- docs/_static/js/bug.js
+++ docs/_static/js/bug.js
@@ -7,15 +7,50 @@ function toggleFeedbackForm() {
     check_login();
 }
 
+function isLoggedInAPI() {
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://jira.opnfv.org/rest/auth/1/session",
+        "method": "POST",
+        "headers": {}
+    }
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    })
+}
+
+function loginAPI(username, password) {
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://jira.opnfv.org/rest/auth/1/session",
+        "method": "POST",
+        "headers": {
+            "content-type": "application/json"
+        },
+        "processData": false,
+        "data": {
+            "username": username,
+            "password": password
+        }
+    }
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    })
+}
+
 function check_login() {
 
     var logged_in = false;
     console.log('checking login');
 
-    var submit_btn= document.getElementById("submit-btn");
-    var login_btn= document.getElementById("login-btn");
+    var submit_btn = document.getElementById("submit-btn");
+    var login_btn = document.getElementById("login-btn");
 
-    // TODO: call JIRA API
+    logged_in = isLoggedInAPI();
 
     if (logged_in) {
         submit_btn.style.display = 'block';
