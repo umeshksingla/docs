3aa7e624e0e0a5fba4a452aa3d9006a325ef497a
Umesh Singla <umeshksingla@gmail.com>
2017-08-23 18:15:34 +0530

    Add create issue API and default parameters

--- docs/_static/js/bug.js
+++ docs/_static/js/bug.js
@@ -1,15 +1,31 @@
 console.log('umesh');
 console.log(window.location.href);
 
-var baseUrl = "https://test-odl-docs.atlassian.net/rest";
+var baseUrl = "https://jira.opnfv.org/rest";
 var authUrl = "/auth/1/session";
+var issueUrl = "/api/2/issue";
 
-function toggleLoginForm(show) {
-    document.getElementById("jira-form").classList.toggle(show);
+var docsProjectID = "10309";
+var issueTypeID = "4"; // for "Improvement"
+var priorityTypeID = "5"; // for "Trivial"
+var username = "";
+
+function toggleLoginForm(s, b) {
+    var h;
+    var jira_form = document.getElementById("jira-form");
+
+    h = (s == "show") ? "hide":"show";
+    jira_form.classList.toggle(s, b);
+    jira_form.classList.toggle(h, !b);
 }
 
-function toggleFeedbackForm(show) {
-    document.getElementById("feedback-form").classList.toggle(show);
+function toggleFeedbackForm(s, b) {
+    var h;
+    var feedback_form = document.getElementById("feedback-form");
+
+    h = (s == "show") ? "hide":"show";
+    feedback_form.classList.toggle(s, b);
+    feedback_form.classList.toggle(h, !b);
 }
 
 function hideFeedbackButton() {
@@ -17,18 +33,20 @@ function hideFeedbackButton() {
     document.getElementById("give-feedback-button").style.display = 'none';
 }
 
-function showFeedbackButton(){
+function showFeedbackButton() {
     document.getElementById("bug-form").style.display = 'none';
     document.getElementById("give-feedback-button").style.display = 'block';
+    toggleFeedbackForm("hide", true);
+    toggleLoginForm("hide", true);
 }
 
 function giveFeedback() {
-    if (check_login()) {
-        toggleFeedbackForm("show");
-        toggleLoginForm("hide");
+    if (isLoggedIn()) {
+        toggleFeedbackForm("show", true);
+        toggleLoginForm("hide", true);
     } else {
-        toggleFeedbackForm("hide");
-        toggleLoginForm("show");
+        toggleFeedbackForm("hide", true);
+        toggleLoginForm("show", true);
     }
     hideFeedbackButton();
 }
@@ -47,11 +65,20 @@ function isLoggedInAPI() {
     })
 }
 
+function isLoggedIn() {
+    var logged_in = false;
+    console.log('checking login');
+
+    //logged_in = isLoggedInAPI();
+    console.log(logged_in);
+    return logged_in;
+}
+
 function loginAPI(username, password) {
     var settings = {
         "async": true,
         "crossDomain": true,
-        "url": "https://jira.opnfv.org/rest/auth/1/session",
+        "url": baseUrl + authUrl,
         "method": "POST",
         "headers": {
             "content-type": "application/json"
@@ -68,17 +95,50 @@ function loginAPI(username, password) {
     })
 }
 
-function check_login() {
-    var logged_in = true;
-    console.log('checking login');
-
-    //logged_in = isLoggedInAPI();
-    console.log(logged_in);
-    return logged_in;
-}
-
 function login() {
+    console.log($('#usr').val())
     // if successful login
     toggleFeedbackForm("show");
     toggleLoginForm("hide");
+}
+
+function createIssueAPI(summary, description) {
+
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": baseUrl + issueUrl,
+        "method": "POST",
+        "headers": {
+        "content-type": "application/json"
+        },
+        "processData": false,
+        "data": {
+            "project":{
+                "id": docsProjectID
+            },
+            "summary": summary,
+            "issuetype":{
+                "id": issueTypeID
+            },
+            "reporter":{
+                "name": username
+            },
+            "priority":{
+                "id": priorityTypeID
+            },
+            "labels":[
+                "website"
+            ],
+            "description": description
+        }
+    }
+
+    $.ajax(settings).done(function (response) {
+      console.log(response);
+    });
+}
+
+function createIssue(argument) {
+
 }
